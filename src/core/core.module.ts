47db3aae5116a4224386f5d546127484251b3395
JSisques <javierplazasisques@gmail.com>
2025-06-14 20:42:04 +0200

    feat: Add forRoot method to CoreModule for dynamic module imports based on application bootstrap options

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,4 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ApplicationBootstrapOptions } from 'src/common/interfaces/application-bootstrap-options.interface';
+import { InMemoryPersistanceModule } from 'src/users/infrastructure/persistence/in-memory/in-memory-persistance.module';
 
 @Module({})
-export class CoreModule {}
+export class CoreModule {
+  static forRoot(options: ApplicationBootstrapOptions) {
+    let imports;
+    switch (options.driver) {
+      case 'in-memory':
+        imports = [InMemoryPersistanceModule];
+        break;
+      default:
+        throw new Error(`Unsupported driver: ${options.driver}`);
+    }
+
+    return {
+      module: CoreModule,
+      imports,
+    };
+  }
+}
