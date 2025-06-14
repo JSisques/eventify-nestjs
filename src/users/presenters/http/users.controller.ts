ea983a2960d185d8fcd4fc725068edfe4f496e4e
JSisques <javierplazasisques@gmail.com>
2025-06-14 20:39:59 +0200

    feat: Add detailed JSDoc comments to UsersController methods for better documentation

--- src/users/presenters/http/users.controller.ts
+++ src/users/presenters/http/users.controller.ts
@@ -14,25 +14,47 @@ import { CreateUserCommand } from 'src/users/application/commands/create-user.co
 import { UpdateUserCommand } from 'src/users/application/commands/update-user.command';
 import { DeleteUserCommand } from 'src/users/application/commands/delete-user.command';
 
+/**
+ * Controller handling HTTP requests for user management
+ */
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  /**
+   * Retrieves all users
+   * @returns A list of all users
+   */
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  /**
+   * Finds a user by their ID
+   * @param id - The unique identifier of the user
+   * @returns The user with the specified ID
+   */
   @Get(':id')
   findById(@Param('id') id: string) {
     return this.usersService.findById(id);
   }
 
+  /**
+   * Finds a user by their email address
+   * @param email - The email address to search for
+   * @returns The user with the specified email address
+   */
   @Get('email/:email')
   findByEmail(@Param('email') email: string) {
     return this.usersService.findByEmail(email);
   }
 
+  /**
+   * Creates a new user
+   * @param createUserDto - The data for creating a new user
+   * @returns The newly created user
+   */
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(
@@ -44,6 +66,11 @@ export class UsersController {
     );
   }
 
+  /**
+   * Updates an existing user
+   * @param updateUserDto - The data for updating the user
+   * @returns The updated user
+   */
   @Patch()
   update(@Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(
@@ -54,6 +81,11 @@ export class UsersController {
     );
   }
 
+  /**
+   * Removes a user
+   * @param id - The ID of the user to remove
+   * @returns The result of the removal operation
+   */
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(new DeleteUserCommand(id));
