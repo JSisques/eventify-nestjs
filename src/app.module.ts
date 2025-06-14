54046e9998879c08d928fc2737259fa12dbf473e
JSisques <javierplazasisques@gmail.com>
2025-06-14 20:44:05 +0200

    feat: Enhance AppModule to support dynamic configuration with ApplicationBootstrapOptions and integrate CqrsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
+import { CqrsModule } from '@nestjs/cqrs';
+import { ApplicationBootstrapOptions } from './common/interfaces/application-bootstrap-options.interface';
+import { UsersModule } from './users/application/users.module';
+import { UsersInfrastructureModule } from './users/infrastructure/users-infrastructure.module';
 
 @Module({
-  imports: [CoreModule],
+  imports: [CqrsModule.forRoot(), CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  static register(options: ApplicationBootstrapOptions) {
+    return {
+      module: AppModule,
+      imports: [
+        CoreModule.forRoot(options),
+        UsersModule.withInfrastructure(
+          UsersInfrastructureModule.use(options.driver),
+        ),
+      ],
+    };
+  }
+}
