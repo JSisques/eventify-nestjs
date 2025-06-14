7fd944ced59c57589a481182c9c87977158586d0
JSisques <javierplazasisques@gmail.com>
2025-06-14 22:09:45 +0200

    refactor: Add documentation to UserNotFoundException to clarify its purpose and constructor parameters

--- src/users/domain/exceptions/user-not-found.exception.ts
+++ src/users/domain/exceptions/user-not-found.exception.ts
@@ -1,4 +1,12 @@
+/**
+ * Exception thrown when a user is not found in the system
+ */
 export class UserNotFoundException extends Error {
+  /**
+   * Creates a new UserNotFoundException instance
+   * @param message - The error message
+   * @param userId - The ID of the user that was not found
+   */
   constructor(
     message: string,
     public readonly userId: string,
