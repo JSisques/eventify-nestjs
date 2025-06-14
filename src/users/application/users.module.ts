45d3d622fbb01d9491f38e77829131fe5b4af99c
JSisques <javierplazasisques@gmail.com>
2025-06-14 19:28:00 +0200

    feat: Enhance UsersModule by adding command handlers and event handlers for user management

--- src/users/application/users.module.ts
+++ src/users/application/users.module.ts
@@ -1,9 +1,28 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module, Type } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { UsersController } from '../presenters/http/users.controller';
+import { CreateUserCommandHandler } from './commands/create-user.command-handler';
+import { DeleteUserCommandHandler } from './commands/delete-user.command-handler';
+import { UserCreatedEventHandler } from './event-handlers/user-created.event-handler';
+import { UserDeletedEventHandler } from './event-handlers/user-deleted.event-handler';
+import { UserFactory } from '../domain/factories/user.factory';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    UserFactory,
+    CreateUserCommandHandler,
+    UserCreatedEventHandler,
+    DeleteUserCommandHandler,
+    UserDeletedEventHandler,
+  ],
 })
-export class UsersModule {}
+export class UsersModule {
+  static withInfrastructure(infrastructureModule: Type | DynamicModule) {
+    return {
+      module: UsersModule,
+      imports: [infrastructureModule],
+    };
+  }
+}
