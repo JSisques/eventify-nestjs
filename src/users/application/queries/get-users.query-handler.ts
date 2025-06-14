2c069c523dbeee26a805572762d646b7f089d98c
JSisques <javierplazasisques@gmail.com>
2025-06-14 19:29:41 +0200

    feat: Add documentation comments to GetUsersQueryHandler for improved clarity

--- src/users/application/queries/get-users.query-handler.ts
+++ src/users/application/queries/get-users.query-handler.ts
@@ -5,12 +5,24 @@ import { User } from 'src/users/domain/user';
 import { Logger } from '@nestjs/common';
 import { UserRepository } from '../ports/user.repository';
 
+/**
+ * Query handler for retrieving all users
+ */
 @QueryHandler(GetUsersQuery)
 export class GetUsersQueryHandler implements IQueryHandler<GetUsersQuery> {
   private readonly logger = new Logger(GetUsersQueryHandler.name);
 
+  /**
+   * Creates a new GetUsersQueryHandler instance
+   * @param userRepository The repository for accessing user data
+   */
   constructor(private readonly userRepository: UserRepository) {}
 
+  /**
+   * Executes the get users query to retrieve all users
+   * @param query The query parameters (empty in this case)
+   * @returns A promise that resolves to an array of users
+   */
   async execute(query: GetUsersQuery): Promise<User[]> {
     this.logger.debug('Executing get users query');
     return this.userRepository.findAll();
