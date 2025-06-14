47dbe13bdc6d2b92569a11871761fe494da3dccc
JSisques <javierplazasisques@gmail.com>
2025-06-14 19:21:43 +0200

    refactor: Update UserCreatedEventHandler to remove UserRepository dependency and add documentation comments

--- src/users/application/event-handlers/user-created.event-handler.ts
+++ src/users/application/event-handlers/user-created.event-handler.ts
@@ -1,16 +1,25 @@
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { UserCreatedEvent } from 'src/users/domain/events/user-created.event';
-import { UserRepository } from '../ports/user.repository';
 import { Logger } from '@nestjs/common';
 
+/**
+ * Event handler for processing user created events
+ */
 @EventsHandler(UserCreatedEvent)
 export class UserCreatedEventHandler
   implements IEventHandler<UserCreatedEvent>
 {
   private readonly logger = new Logger(UserCreatedEventHandler.name);
 
-  constructor(private readonly userRepository: UserRepository) {}
+  /**
+   * Creates a new UserCreatedEventHandler instance
+   */
+  constructor() {}
 
+  /**
+   * Handles the user created event
+   * @param event The user created event containing the created user details
+   */
   async handle(event: UserCreatedEvent): Promise<void> {
     this.logger.debug(
       `Processing user created event: ${JSON.stringify(event)}`,
