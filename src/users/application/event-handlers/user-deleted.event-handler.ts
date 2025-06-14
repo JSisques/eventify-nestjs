2a139d10855afb6b1b23c16847be197f5e7f4a34
JSisques <javierplazasisques@gmail.com>
2025-06-14 19:23:20 +0200

    feat: Implement UserDeletedEventHandler to process user deleted events and log details

--- src/users/application/event-handlers/user-deleted.event-handler.ts
+++ src/users/application/event-handlers/user-deleted.event-handler.ts
@@ -1,28 +1,30 @@
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { UserCreatedEvent } from 'src/users/domain/events/user-created.event';
 import { Logger } from '@nestjs/common';
+import { UserDeletedEvent } from 'src/users/domain/events/user-deleted.event';
 
 /**
- * Event handler for processing user created events
+ * Event handler for processing user deleted events
  */
-@EventsHandler(UserCreatedEvent)
-export class UserCreatedEventHandler
-  implements IEventHandler<UserCreatedEvent>
+@EventsHandler(UserDeletedEvent)
+export class UserDeletedEventHandler
+  implements IEventHandler<UserDeletedEvent>
 {
-  private readonly logger = new Logger(UserCreatedEventHandler.name);
+  private readonly logger = new Logger(UserDeletedEventHandler.name);
 
   /**
-   * Creates a new UserCreatedEventHandler instance
+   * Creates a new UserDeletedEventHandler instance
    */
   constructor() {}
 
   /**
-   * Handles the user created event
-   * @param event The user created event containing the created user details
+   * Handles the user deleted event
+   * @param event The user deleted event containing the deleted user details
+   * @returns A promise that resolves when the event has been handled
    */
-  async handle(event: UserCreatedEvent): Promise<void> {
+  async handle(event: UserDeletedEvent): Promise<void> {
     this.logger.debug(
-      `Processing user created event: ${JSON.stringify(event)}`,
+      `Processing user deleted event: ${JSON.stringify(event)}`,
     );
   }
 }
