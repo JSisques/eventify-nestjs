923bcfd0a300fe030d7b442235d3d83264508abc
JSisques <javierplazasisques@gmail.com>
2025-06-14 21:47:19 +0200

    feat: Add global exception filter to improve error handling across the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,19 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { ApplicationBootstrapOptions } from './common/interfaces/application-bootstrap-options.interface';
 import { UsersModule } from './users/application/users.module';
 import { UsersInfrastructureModule } from './users/infrastructure/users-infrastructure.module';
+import { APP_FILTER } from '@nestjs/core';
+import { DomainExceptionFilter } from 'shared/infrastructure/filters/domain-exception.filter';
 
 @Module({
   imports: [CqrsModule.forRoot(), CoreModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: DomainExceptionFilter,
+    },
+  ],
 })
 export class AppModule {
   static register(options: ApplicationBootstrapOptions) {
