52492b34172ee4c8ea22c63889205125dae0173e
JSisques <javierplazasisques@gmail.com>
2025-06-14 22:10:04 +0200

    refactor: Add documentation for UserPrimitives to clarify its structure and properties

--- src/users/domain/primitives/user-primitives.ts
+++ src/users/domain/primitives/user-primitives.ts
@@ -1,3 +1,11 @@
+/**
+ * Represents the primitive values that make up a User entity
+ * @typedef {Object} UserPrimitives
+ * @property {string} id - The unique identifier of the user
+ * @property {string} name - The name of the user
+ * @property {string} email - The email address of the user
+ * @property {string} password - The hashed password of the user
+ */
 export type UserPrimitives = {
   id: string;
   name: string;
