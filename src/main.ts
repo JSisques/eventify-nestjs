3b2aeb6def73c953446851d39059776cdcda9cb1
JSisques <javierplazasisques@gmail.com>
2025-06-15 10:35:25 +0200

    refactor: Simplify application bootstrap options by using a dedicated object for configuration

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { ApplicationBootstrapOptions } from './common/interfaces/application-bootstrap-options.interface';
 
 async function bootstrap() {
   const logger = new Logger('Main');
 
-  const app = await NestFactory.create(
-    AppModule.register({ driver: 'in-memory', cacheDriver: 'in-memory' }),
-  );
+  const options: ApplicationBootstrapOptions = {
+    driver: 'in-memory',
+    cacheDriver: 'in-memory',
+  };
+
+  const app = await NestFactory.create(AppModule.register(options));
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
