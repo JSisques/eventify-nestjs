7b7e12aa04558472f54c1cebef06c0071bd9d8be
JSisques <javierplazasisques@gmail.com>
2025-06-15 21:48:11 +0200

    feat: Add fromPrimitives and toPrimitives methods in Auth class for better serialization and deserialization of authentication data

--- src/auth/domain/auth.ts
+++ src/auth/domain/auth.ts
@@ -1,3 +1,4 @@
+import { AuthPrimitives } from './primitives/auth-primitives';
 import { AuthProvider } from './value-objects/auth-provider';
 
 export class Auth {
@@ -13,4 +14,34 @@ export class Auth {
     public failedLoginAttempts?: number,
     public isLocked?: boolean,
   ) {}
+
+  static fromPrimitives(primitives: AuthPrimitives): Auth {
+    return new Auth(
+      primitives.id,
+      primitives.userId,
+      AuthProvider.create(primitives.provider),
+      primitives.hashedPassword,
+      primitives.refreshToken,
+      primitives.mfaEnabled,
+      primitives.mfaSecret,
+      primitives.lastLoginAt,
+      primitives.failedLoginAttempts,
+      primitives.isLocked,
+    );
+  }
+
+  toPrimitives(): AuthPrimitives {
+    return {
+      id: this.id,
+      userId: this.userId,
+      provider: this.provider.value.toString(),
+      hashedPassword: this.hashedPassword,
+      refreshToken: this.refreshToken,
+      mfaEnabled: this.mfaEnabled,
+      mfaSecret: this.mfaSecret,
+      lastLoginAt: this.lastLoginAt,
+      failedLoginAttempts: this.failedLoginAttempts,
+      isLocked: this.isLocked,
+    };
+  }
 }
