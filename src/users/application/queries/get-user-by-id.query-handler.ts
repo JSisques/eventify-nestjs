e89fa40b6ba43ffba4ffad84be3de8bb17a8eb8f
JSisques <javierplazasisques@gmail.com>
2025-06-15 10:55:13 +0200

    feat: Implement caching in GetUserByIdQueryHandler to improve user retrieval efficiency

--- src/users/application/queries/get-user-by-id.query-handler.ts
+++ src/users/application/queries/get-user-by-id.query-handler.ts
@@ -3,6 +3,8 @@ import { GetUserByIdQuery } from './get-user-by-id.query';
 import { User } from 'src/users/domain/user';
 import { Logger } from '@nestjs/common';
 import { UserRepository } from '../ports/user.repository';
+import { UserCacheRepository } from '../ports/user-cache.repository';
+import { UserNotFoundException } from 'src/users/domain/exceptions/user-not-found.exception';
 
 /**
  * Query handler for retrieving a user by their ID
@@ -16,8 +18,12 @@ export class GetUserByIdQueryHandler
   /**
    * Creates a new GetUserByIdQueryHandler instance
    * @param userRepository The repository for accessing user data
+   * @param userCacheRepository The repository for accessing cached user data
    */
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly userCacheRepository: UserCacheRepository,
+  ) {}
 
   /**
    * Executes the get user by ID query to retrieve a specific user
@@ -25,7 +31,18 @@ export class GetUserByIdQueryHandler
    * @returns A promise that resolves to the requested user
    */
   async execute(query: GetUserByIdQuery): Promise<User> {
-    this.logger.debug('Executing get user by id query');
-    return this.userRepository.findById(query.id);
+    this.logger.debug(`Executing get user by id query: ${query.id}`);
+
+    this.logger.debug('Checking cached user');
+    const cachedUser = await this.userCacheRepository.getUserById(query.id);
+    if (cachedUser) return cachedUser;
+
+    this.logger.debug('Fetching from user repository');
+    const user = await this.userRepository.findById(query.id);
+    if (!user)
+      throw new UserNotFoundException(`User with id ${query.id} not found`);
+
+    await this.userCacheRepository.setUser(user);
+    return user;
   }
 }
